df6e470f3326af43ad68f833ed4e57480d259cdf
root <cagdas.oztekin@ug.bilkent.edu.tr>
2016-08-04 16:37:51 -0400

    Update.

--- index.php
+++ index.php
@@ -2,24 +2,9 @@
         <head>
                 <meta id="meta" name="viewport"
                 content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" type="text/css" href="shortstyle.css">
+		<link rel="stylesheet" type="text/css" href="./shortstyle.css">
         </head>
-        <body background="../short.jpg">
-	<?php
-	    $servername = "";
-	    $username = "";
-	    $password = "";
-        $dbname = "short";
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-        //    die("Connection failed: " . $conn->connect_error);
-        }
-        else{
-            mysqli_query($conn, "INSERT INTO short VALUES (".$_SERVER['HTTP_USER_AGENT'].", ".$_SERVER['HTTPS'].", ".$_SERVER['REMOTE_ADDR'].", ".$_SERVER['REMOTE_HOST'].", ".$_SERVER['REMOTE_PORT'].", ".$_SERVER['REMOTE_USER'].", ".$_SERVER['REDIRECT_REMOTE_USER'].", NOW(), ".$_SERVER['HTTP_REFERER'].");");
-        }
-        mysqli_close($conn);
-            
-    ?> 
+        <body background="./short.png">
 
 
             <form action="shortened.php" method="post">
